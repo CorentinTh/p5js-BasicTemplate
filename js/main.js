59acae1fe1ba99701af9e0be076229ecbf923e49
CorentinTh <corentin.thomasset74@gmail.com>
2017-10-14 00:18:50 +0300

    Adding colors

--- js/main.js
+++ js/main.js
@@ -6,8 +6,14 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
+//   Some colors :
+// Darck grey #222831
+// Medium grey #393E46
+// Cyan #00ADB5
+// Light grey #EEEEEE
+
 function draw() {
-    background(0);
+    background("#222831");
 }
 
 function windowResized() {
