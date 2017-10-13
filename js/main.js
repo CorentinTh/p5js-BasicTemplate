e656fdd17fa406361d7892ffa7b8d6dbc2058aea
CorentinTh <corentin.thomasset74@gmail.com>
2017-10-14 00:11:23 +0300

    Basic template

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Corentin THOMASSET on 13/10/2017.
+ */
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
